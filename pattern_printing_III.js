140f12926deac1b7f91d3ed71f4ac37e49e5add4
smriti-67-samanta <samantasmriti0@gmail.com>
2025-02-24 23:17:22 +0530

    Add files via upload

---
+++ pattern_printing_III.js
@@ -0,0 +1,19 @@
+let bag=""
+for(let i=1; i<5; i++){
+  bag=bag+"*"+" " 
+}
+bag=bag+"*"
+console.log(bag)
+
+for(let i=1; i<8; i++){
+  if(i%2==0){
+  
+  console.log("*")
+}
+else{
+  console.log(" ")
+}
+
+
+}
+console.log(bag)
