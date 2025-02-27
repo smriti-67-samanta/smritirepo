c4d5348fe32d871de46bf522c582e31f4b310c2d
smriti-67-samanta <samantasmriti0@gmail.com>
2025-02-27 21:20:22 +0530

    Add files via upload

---
+++ object method for sentence generation.js
@@ -0,0 +1,36 @@
+let sentenceBuilder = {
+    subject: "I",
+    verb: "am",
+    object: "coding",
+
+    buildSentence: function() {
+        return this.subject && this.verb && this.object
+            ? this.subject + " " + this.verb + " " + this.object
+            : "Incomplete sentence";
+    },
+
+    updateProperty: function(property, value) {
+        if (this.hasOwnProperty(property)) {
+            this[property] = value;
+            
+            if (property === "subject" && this.subject !== "I") {
+                this.verb = "is";
+            } else if (property === "subject" && this.subject === "I") {
+                this.verb = "am";
+            }
+            return value;
+        } else {
+            return "Invalid property";
+        }
+    }
+};
+
+console.log(sentenceBuilder.buildSentence()); 
+
+sentenceBuilder.updateProperty("verb", "am learning");
+console.log(sentenceBuilder.buildSentence()); 
+
+sentenceBuilder.updateProperty("subject", "The cat");
+console.log(sentenceBuilder.buildSentence()); 
+
+console.log(sentenceBuilder.updateProperty("mood", "happy")); 
