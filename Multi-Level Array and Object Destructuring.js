115e7c452c05e7227f667e46e56c8d371e85c750
smriti-67-samanta <samantasmriti0@gmail.com>
2025-02-28 10:54:45 +0530

    Add files via upload

---
+++ Multi-Level Array and Object Destructuring.js
@@ -0,0 +1,15 @@
+let people = [ { name: "Alice",
+                   address: { city: "New York", 
+                      street: { name: "Broadway", 
+                          number: 123 } } }, 
+                   
+                   
+                 { name: "Bob", 
+                   address: { city: "Los Angeles", 
+                        street: { name: "Sunset Boulevard", 
+                           number: 456 } } } ]
+                           
+  let res=people.map(({ name, address: { city, street: { name: streetName } } }) => {
+               return `${name} lives in ${city} on ${streetName}`;
+  });
+  console.log(res)
