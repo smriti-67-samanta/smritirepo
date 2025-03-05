2f4dbc332c0fa4e3f1a7ec12ca7bd530ae84076e
smriti-67-samanta <samantasmriti0@gmail.com>
2025-03-06 00:06:16 +0530

    Add files via upload

---
+++ Debugging an Array and String Manipulation Program.js
@@ -0,0 +1,19 @@
+function manageStudents(students) {
+    //let students = ["Alice", "Bob", "Charlie"];
+
+    // Add "David" at index 1
+    students.splice(1, 0, "David");
+
+    // Check if "Eve" is in the list
+    console.log(students.includes("Eve"));  // Should return false
+
+    // Convert the array to a string with names separated by commas
+    console.log(students.join(","));  // Expected: "Alice,David,Bob,Charlie"
+}
+
+
+manageStudents(["Alice", "Bob", "Charlie"]);
+
+manageStudents(["Aman", "Boby", "Charls"]);
+manageStudents(["Amar", "Eve", "Ritu"]);
+manageStudents(["Ramesh", "Bobs", "Chiru"]);
