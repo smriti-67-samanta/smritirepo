cb1b6e22f77da76578fc5ff9808e3b5e049916b9
smriti-67-samanta <samantasmriti0@gmail.com>
2025-03-07 10:47:55 +0530

    Add files via upload

---
+++ Debugging Incorrect Closure Value Retention.js
@@ -0,0 +1,28 @@
+function createFunctionList() {
+
+let functions = [];
+
+for (var i = 0; i < 5; i++) {
+  (function (i) {
+
+functions.push(function () {
+
+console.log("Index:", i);
+
+});
+})(i)
+}
+
+return functions; }
+
+const functionList = createFunctionList();
+
+functionList[0]();    // Expected Output: "Index: 0", Actual Output: "Index: 5"
+
+functionList[1]();   // Expected Output: "Index: 1", Actual Output: "Index: 5"
+
+functionList[2]();   // Expected Output: "Index: 2", Actual Output: "Index: 5"
+
+functionList[3]();   // Expected Output: "Index: 3", Actual Output: "Index: 5"
+
+functionList[4]();   // Expected Output: "Index: 4", Actual Output: "Index: 5"
