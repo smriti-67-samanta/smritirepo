5e6bb4c91b4a93d8d92824218c3f43c246bf1414
smriti-67-samanta <samantasmriti0@gmail.com>
2025-03-07 10:42:06 +0530

    Add files via upload

---
+++ Encapsulating Bank Account using Closures.js
@@ -0,0 +1,30 @@
+function createBankAccount(initialBalance) {
+    let balance = initialBalance; 
+
+    return {
+        deposit: function(amount) {
+            if (amount > 0) {
+                balance += amount;
+                return balance;
+            } 
+        },
+        withdraw: function(amount) {
+            if (amount > 0) {
+                if (balance >= amount) {
+                    balance -= amount;
+                    return balance;
+                } 
+            } 
+        },
+        getBalance: function() {
+            return balance;
+        }
+    };
+}
+
+
+const account = createBankAccount(100);
+
+console.log(account.deposit(50)); 
+console.log(account.withdraw(30)); 
+console.log(account.getBalance());
